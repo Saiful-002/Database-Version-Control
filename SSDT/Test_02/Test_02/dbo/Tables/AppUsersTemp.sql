9d54c33831a643e35a689015af9356e1b6103fc5
Saiful Raihan <saifulraihan321@gmail.com>
2024-10-24 12:06:31 +0600

    Added AppUsersTemp table

---
+++ SSDT/Test_02/Test_02/dbo/Tables/AppUsersTemp.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[AppUsersTemp] (
+    [Id]             UNIQUEIDENTIFIER NOT NULL,
+    [Name]           NVARCHAR (MAX)   NULL,
+    [UserName]       NVARCHAR (MAX)   NULL,
+    [Password]       NVARCHAR (MAX)   NULL,
+    [EmailAddress]   NVARCHAR (MAX)   NULL,
+    [IsActive]       BIT              NULL,
+    [RoleId]         INT              NOT NULL,
+    [IsDeleted]      BIT              NULL,
+    [CreatedDate]    DATETIME2 (7)    NOT NULL,
+    [UpdatedDate]    DATETIME2 (7)    NOT NULL,
+    [CreatedBy]      UNIQUEIDENTIFIER NOT NULL,
+    [LastModifiedBy] UNIQUEIDENTIFIER NOT NULL,
+    [Status]         INT              NOT NULL,
+    [Salt]           NVARCHAR (500)   NULL
+);
+
